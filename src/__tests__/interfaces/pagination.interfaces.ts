373d7a25f3231cbda242e9d003d39815bd7870da
Arthur Meier <Arthurcmeier@outlook.com>
2023-05-06 20:44:42 -0300

    little fix

--- src/__tests__/interfaces/pagination.interfaces.ts
+++ src/__tests__/interfaces/pagination.interfaces.ts
@@ -1,19 +1,19 @@
-import { Movie } from '../../entities'
+import { Movie } from "../../entities";
 
 interface iPagination {
-    prevPage: string | null
-    nextPage: string | null
-    count: number
-    data: Movie[]
+  prevPage: string | null;
+  nextPage: string | null;
+  count: number;
+  data: Movie[];
 }
 
 interface iPaginationParams {
-    page: number
-    perPage: number
-    order: string
-    sort: string
-    prevPage: string | null
-    nextPage: string | null
+  page: number;
+  perPage: number;
+  order: string;
+  sort: string;
+  prevPage: string | null;
+  nextPage: string | null;
 }
 
-export { iPagination, iPaginationParams }
+export { iPagination, iPaginationParams };
